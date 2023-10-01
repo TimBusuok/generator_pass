9a31fc35be01e8d80add000f6dd6db87fdf0c0be
Тимофей <timone300003@gmail.com>
2023-10-01 22:35:29 +0300

    создали генератор паролей

---
+++ program.py
@@ -0,0 +1,10 @@
+import random
+
+def generator_pass(lenght):
+    symbols = "abcdefghiklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+    password = "".join(random.sample(symbols,lenght))
+    return password
+
+
+pass_lenght = int(input("Введите размер вашего желаемого пароля: "))
+print("Вот твой пароль!!: " + generator_pass(pass_lenght))
